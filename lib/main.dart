a3c567002ab44425771b618413ff1b9d14c8e11c
Gustavo Silva <gustavos_barros@hotmail.com>
2021-11-15 20:54:37 -0300

    objeto Transactions

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,12 @@ class MyHomePage extends StatelessWidget {
       amount: 69.99,
       date: DateTime.now(),
     ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
   ];
 
   @override
@@ -43,9 +49,12 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            color: Colors.red,
-            child: Text("LIST OF TX"),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Text(tx.title),
+              );
+            }).toList(),
           ),
         ],
       ),
